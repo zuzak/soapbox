d9a98121779027397bf443f281372ee03ffc63df
Douglas Gardner <douglas@chippy.ch>
2016-09-20 17:21:29 +0000

    Re-restrict hosts

--- utils.js
+++ utils.js
@@ -1,6 +1,6 @@
 module.exports = {
 	isValidHost: function ( host ) {
-		return true;
+		//return true;
 		var regex = /[a-z]{3}[0-9]\.pip\.aber\.ac.\.uk/;
 		var match = host.match( regex );
 		return Boolean( match );
