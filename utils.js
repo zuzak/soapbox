00215ea6ee307c53d64a2bb5f173242b401f063e
Douglas Gardner <douglas@chippy.ch>
2016-09-23 20:18:29 +0000

    Lazily commit everything

--- utils.js
+++ utils.js
@@ -1,7 +1,6 @@
 module.exports = {
 	isValidHost: function ( host ) {
-		//return true;
-		var regex = /[a-z]{3}[0-9]\.pip\.aber\.ac.\.uk/;
+		var regex = /[a-z]{3}[0-9]*\.pip\.aber\.ac\.uk/;
 		var match = host.match( regex );
 		return Boolean( match );
 	}
